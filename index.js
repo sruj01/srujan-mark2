d2c66157e82e93ca07fe6ab58850a7bf40de6774
srujan <srujan001ssa@gmail.com>
2021-09-04 03:04:11 +0530

    added question numbers

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var userChoice = readlineSync.question(chalk.black.bgMagenta(" Hey There! ")+"\n
 
 if (userChoice == 'Y'|| userChoice=='y')
 {
-  var username= readlineSync.question("Enter your name and we'll get started:  ")
+  var username= readlineSync.question("\nEnter your name and we'll get started:  ")
 
   function play(question,answer,options,fact)
   {
@@ -28,17 +28,17 @@ if (userChoice == 'Y'|| userChoice=='y')
 
   var questions=[
 
-     {question: 'What color do you think is the universe as a whole said to be?', options: ['Black','Black Chocolate','Cosmic Latte\'','Alien Black'], answer: 3, fact:'It has been declared by scientists that the average color of Cosmos resembles a Latte\'. That must look Tasty!!'},
+     {question: 'Q1. What color do you think is the universe as a whole said to be?', options: ['Black','Black Chocolate','Cosmic Latte\'','Alien Black'], answer: 3, fact:'It has been declared by scientists that the average color of Cosmos resembles a Latte\'. That must look Tasty!!'},
 
-     {question: 'Ever wondered where the elemets that are necessary for life come from?', options: ['From a Dying Star','The Big Bang','Blackholes','Inexplicable' ], answer : 1, fact:'When a star’s core runs out of hydrogen, the star begins to die out. The dying star expands into a red giant, and this now begins to manufacture carbon atoms by fusing helium atoms.'},
+     {question: 'Q2. Ever wondered where the elemets that are necessary for life come from?', options: ['From a Dying Star','The Big Bang','Blackholes','Inexplicable' ], answer : 1, fact:'When a star’s core runs out of hydrogen, the star begins to die out. The dying star expands into a red giant, and this now begins to manufacture carbon atoms by fusing helium atoms.'},
 
-     {question: 'What do you think is the average temperature of our Cosmos..', options: ['0° Celsius','100° Celsius','-100° Celsius','-270° Celsius'], answer : 4, fact:'-270° Celsius!! To put things in perspective, the temperature of Liquid Nitrogen(coldest substance on Earth) is -196° Celsius. Now that is tough to beat!'},
+     {question: 'Q3. What do you think is the average temperature of our Cosmos..', options: ['0° Celsius','100° Celsius','-100° Celsius','-270° Celsius'], answer : 4, fact:'-270° Celsius!! To put things in perspective, the temperature of Liquid Nitrogen(coldest substance on Earth) is -196° Celsius. Now that is tough to beat!'},
 
-     {question: '\'An ocean of water floating in outer space!?\'..YEP. How much of Earth\'s ocean would it amount to...? ', options: ['A million times','A billion times','100 billion times','A LOT MORE'], answer : 4, fact:'The water is equivalent to 140 trillion times all the water in the oceans, and it could provide every single person on Earth a planet’s worth of water 20,000 times over. This is an almost incomprehensible amount of water'},
+     {question: 'Q4. \'An ocean of water floating in outer space!?\'..YEP. How much of Earth\'s ocean would it amount to...? ', options: ['A million times','A billion times','100 billion times','A LOT MORE'], answer : 4, fact:'The water is equivalent to 140 trillion times all the water in the oceans, and it could provide every single person on Earth a planet’s worth of water 20,000 times over. This is an almost incomprehensible amount of water'},
 
-     {question: 'What percentage of the universe does matter/energy constitute? Hmmm..', options: ['30%','20%','5%'] , answer: 3, fact:'Only about 4.9% of the mass-energy of the Universe is atoms, and of that, only half has been spotted with telescopes'} ,
+     {question: 'Q5. What percentage of the universe does matter/energy constitute? Hmmm..', options: ['30%','20%','5%'] , answer: 3, fact:'Only about 4.9% of the mass-energy of the Universe is atoms, and of that, only half has been spotted with telescopes'} ,
 
-     {question: 'Ever think about alien life? ', options: ['Yes','No'], answer : 1, fact:'Ofcourse you have, regardless of your answer.\nDo they exist:  Most probably\nCan you meet them:  Very Unlikely'}
+     {question: 'Q6. Ever think about alien life? ', options: ['Yes','No'], answer : 1, fact:'Ofcourse you have, regardless of your answer.\nDo they exist:  Most probably\nCan you meet them:  Very Unlikely'}
 
      ]
 
